74c30a3ee3cef04cc1872259c2973a5c2e5bcb49
abhisingh197 <abhisingh29aks@gmail.com>
2024-11-30 19:39:40 +0530

    Added "Reset" button in component and it passed

--- my-app/src/counter/Counter.js
+++ my-app/src/counter/Counter.js
@@ -3,10 +3,14 @@ import React, { useState } from "react";
 function Counter() {
   const [count, setCount] = useState(0);
 
+  const increment = () => setCount((prevCount) => prevCount + 1);
+  const reset = () => setCount(0); // Add reset logic.
+
   return (
     <div>
       <p>Count: {count}</p>
-      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <button onClick={increment}>Increment</button>
+      <button onClick={reset}>Reset</button> {/* Add Reset button */}
     </div>
   );
 }
